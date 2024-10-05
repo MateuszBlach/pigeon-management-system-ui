73978ae444e5a3c6b7d0722d26acf16bbd61b435
Mateusz <mateusz.blach@interia.pl>
2024-10-05 10:22:30 +0200

    add basic ag-grid

--- src/app/components/pigeon/pigeons-main-page.component.ts
+++ src/app/components/pigeon/pigeons-main-page.component.ts
@@ -1,26 +1,35 @@
-import {Component, OnInit} from "@angular/core";
-import {PigeonService} from "../../services/pigeon/pigeon.service";
-import {PigeonDTO} from "../../dto/pigeon.dto";
-import {AuthService} from "../../services/auth/auth.service";
-import {AddPigeonComponent} from "./add-pigeon/add-pigeon.component";
+import { Component, OnInit } from "@angular/core";
+import { PigeonService } from "../../services/pigeon/pigeon.service";
+import { PigeonDTO } from "../../dto/pigeon.dto";
+import { AuthService } from "../../services/auth/auth.service";
+import { AddPigeonComponent } from "./add-pigeon/add-pigeon.component";
+import { AgGridAngular } from "ag-grid-angular";
+import { ColDef } from "ag-grid-community"; // Import ColDef
 
 @Component({
   selector: "pigeon",
   standalone: true,
   imports: [
-    AddPigeonComponent
+    AddPigeonComponent,
+    AgGridAngular
   ],
   templateUrl: "./pigeons-main-page.component.html"
 })
-export  class PigeonsMainPageComponent implements OnInit {
-
+export class PigeonsMainPageComponent implements OnInit {
   pigeons: PigeonDTO[] = [];
+
+  // Explicitly type the columnDefs array as ColDef[]
+  columnDefs: ColDef[] = [
+    { headerName: 'Numer obrączki', field: 'ring' },
+    { headerName: 'Płeć', field: 'gender' },
+    { headerName: 'Kolor oczu', field: 'eyeColor' },
+    { headerName: 'Kolor upierzenia', field: 'plumageColor' },
+  ];
 
   constructor(
     private pigeonService: PigeonService,
     private authService: AuthService,
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
     let loggedUserId: number;
@@ -36,9 +45,8 @@ export  class PigeonsMainPageComponent implements OnInit {
         this.pigeons = response;
       },
       error => {
-        this.pigeons = []
+        this.pigeons = [];
       }
-    )
+    );
   }
-
 }
